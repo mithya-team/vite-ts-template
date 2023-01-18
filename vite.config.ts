7aab37b2e93adf69442b0126627cf54e1b168028
aniruddha-mithya <aniruddha@mithyalabs.com>
2023-01-18 14:12:36 +0530

    Add vite-tsconfig-paths to allow using absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsConfigPaths()],
   server: {
     port: 4000,
     host: true, // Will expose this server on local network at <local.ip.add.ress>:4000 Can be useful for testing on real devices (mobile, tablet, second desktop with different os, etc)
